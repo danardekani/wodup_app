b7d6cd4b273f94c6f425f2585c19c8e77459e010
Daniel <ardekanicapital@gmail.com>
2019-08-25 16:32:55 -0400

    Create if else statement to show leaderboard as default and then refine leaderboard results, on filter parameters. Update index view to allow users to dynamically filter leaderboard

--- app/controllers/api/user_workouts_controller.rb
+++ app/controllers/api/user_workouts_controller.rb
@@ -1,7 +1,11 @@
 class Api::UserWorkoutsController < ApplicationController
   #user_workouts create method found in workout_controller
   def index
-    @user_workouts = UserWorkout.where(workout_id:43).order("weight" => "desc")
-    render 'index.json.jb'
+    if @user_workouts = UserWorkout.all
+      render 'index.json.jb'
+    else
+      @user_workouts = UserWorkout.where(workout_id: params[:workout_id]).order("weight" => "desc")
+      render 'index.json.jb'
+    end
   end
 end
