a1db104f951729246b05f82d4a865f5e4991f355
Daniel <ardekanicapital@gmail.com>
2019-08-26 22:42:21 -0400

    fix user_workout controller

--- app/controllers/api/user_workouts_controller.rb
+++ app/controllers/api/user_workouts_controller.rb
@@ -1,11 +1,12 @@
 class Api::UserWorkoutsController < ApplicationController
   #user_workouts create method found in workout_controller
   def index
-    if @user_workouts = UserWorkout.all
-      render 'index.json.jb'
-    else
+    if params[:workout_id] 
       @user_workouts = UserWorkout.where(workout_id: params[:workout_id]).order("weight" => "desc")
-      render 'index.json.jb'
+    else
+      @user_workouts = UserWorkout.all
     end
+    render 'index.json.jb'
   end
 end
+  
