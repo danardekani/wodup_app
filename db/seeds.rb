28337b9f0243765fe5efca8400d45ebbac837f0d
Daniel <ardekanicapital@gmail.com>
2019-08-28 23:40:51 -0400

    Add new workouts in seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,32 +1,11 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 
-require 'faker'
-include Faker
 # Examples:
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-# 500.times do |user|
-#   User.create(
-#     first_name: Faker::Name.first_name,
-#     last_name: Faker::Name.last_name,
-#     email: Faker::Internet.email,
-#     birth_date: Faker::Date.birthday,
-#     height: Faker::Measurement.height,
-#     weight: Faker::Measurement.weight,  
-#     user_name: Faker::Internet.username,
-#     password: Faker::Internet.password,
-#     address: Faker::Address.full_address,
-#     business: Faker::TvShows::Seinfeld.business
-#     # gym: Faker::Seinfeld.business
-#     # gym: Faker::Company.profession
-#   )
-# end
-
-userworkout = UserWorkout.create!(user_id: 22, workout_id: 43, weight: 200, reps: 1 )
-userworkout = UserWorkout.create!(user_id: 23, workout_id: 43, weight: 300, reps: 1 )
-userworkout = UserWorkout.create!(user_id: 24, workout_id: 43, weight: 400, reps: 1 )
-
-
+# userworkout = UserWorkout.create!(user_id: 1109, workout_id: 100, weight: 400, reps: 1 )
+# workout = Workout.create!(name: "Front Squat", workout_type: "Strength" )
+workout = Workout.create!(name: "Back Squat", workout_type: "Strength")
